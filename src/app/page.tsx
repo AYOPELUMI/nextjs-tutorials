6eb05a168fc72b5dd8588d9aed2aef4dbf167a2a
ayopelumi <ayopelumi2014@gmail.com>
2024-02-06 06:29:58 -0800

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="h-screen w-screen flex items-center justify-center flex-col gap-2">
+      <div className="size-fit rounded-xl border border-transparent p-1 px-6 text-metal bg-tahiti font-bold">
+        IMPORT
+      </div>
+      <div className="size-fit rounded-sm border border-transparent p-1 px-6 text-metal bg-tahiti font-bold">
+        SEA FREIGHT FCL
+      </div>
+      <div className="size-fit rounded-sm border border-transparent p-1 px-8 text-custom-green bg-custom-green-bg font-bold">
+        COMPLETED
+      </div>
+    </div>
+ 
+  );
+}
